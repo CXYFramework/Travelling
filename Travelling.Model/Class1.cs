f4ce75fa937d607c25a031bcab41f7e413a0a4ad
SunCode <chenxiaoyangal@163.com>
2015-05-04 18:12:38 +0800

    index

--- Travelling.Model/Class1.cs
+++ Travelling.Model/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Travelling.Model
+namespace Travelling.Models
 {
     public class Class1
     {
--- Travelling.SearchIndexBuilder/HotelDescriptionBuilder.cs
+++ Travelling.SearchIndexBuilder/HotelDescriptionBuilder.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Travelling.SearchIndexBuilder
 {
-    public class Class1
+    public class HotelDescriptionBuilder
     {
+
     }
 }
