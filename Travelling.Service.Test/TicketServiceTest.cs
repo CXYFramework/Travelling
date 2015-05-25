d8ea59376a17d5c2215e206d07144d2db9020bd2
SunCode <chenxiaoyangal@163.com>
2015-05-25 16:46:14 +0800

    Ticket

---
+++ Travelling.Service.Test/TicketServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Travelling.Services;
+
+namespace Travelling.Service.Test
+{
+    [TestClass]
+    public class TicketServiceTest
+    {
+        [TestMethod]
+        public void GetTicketBySearchTestMethod()
+        {
+            TicketService service = new TicketService();
+            string ticketXml = service.GetTicketBySearch(1, "");
+            System.IO.File.AppendAllText("d:\\jjj.xml", ticketXml);
+
+            Assert.IsNotNull(ticketXml);
+        }
+    }
+}
---
+++ Travelling.Services/TicketService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travelling.OpenApiLogic;
+namespace Travelling.Services
+{
+    public class TicketService
+    {
+        OTATicketServiceLogic otaTicketService = new OTATicketServiceLogic();
+        public string GetTicketBySearch(int areaId, string keywords)
+        {
+            return otaTicketService.GetTicketBySearch(areaId, keywords);
+        }  
+    }
+}
