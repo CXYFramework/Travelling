440e79a33ec0e35ab2390d2ee420304091e9cb10
Sinkee <guoenma@gmail.com>
2015-05-26 15:37:04 +0800

    Use model access data in db.

--- Travelling.Web/Controllers/HomeController.cs
+++ Travelling.Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Travelling.Data;
+using Travelling.Model.DTO;
 
 namespace Travelling.Web.Controllers
 {
@@ -10,7 +12,10 @@ namespace Travelling.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            DB_HotelHelper helper = new DB_HotelHelper();
+            var HotelList = helper.GetHotelDescription().ToList<HotelDescriptionDTO>().Take(10);
+
+            return View(HotelList);
         }
 
         public ActionResult About()
